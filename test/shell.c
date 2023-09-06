cce999a0c291550c278e0cf99bb6c18adefce0b5
Malika Oubilla <malikaoubilla@gmail.com>
2023-09-06 08:46:23 -0700

    super simple shell

--- test/shell.c
+++ test/shell.c
@@ -4,25 +4,29 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+
+#define DELIMITERS " \n\t\r"
+#define ARGS 10
+#define ARGS_SIZE 10
+
 char **token(char *str)
 {
 	char **token;
-	int i = 0, j;
-	const int args = 10, size_arg = 10;
+	int i = 1, j;
 	
-	token = (char**)malloc(args * sizeof(char*));
-	for (j = 0; j < args; j++)
-		token[j] = malloc(size_arg);
-	/* get the first token */
-	token[0] = strtok(str, " ");
-	printf("token : %s\n",token[i]);
+	token = (char**)malloc(ARGS * sizeof(char*));
+	for (j = 0; j < ARGS; j++)
+		token[j] = malloc(ARGS_SIZE);
+	/* we get the first token */
+	token[0] = strtok(str, DELIMITERS);
 
-	/* walk through other tokens */
-	while( token != NULL && i < 19)
+	/* then the other tokens */
+	while( i < ARGS)
 	{
+		token[i] = strtok(NULL, DELIMITERS);
+		if (token[i] == NULL)
+			break;
 		i++;
-		token[i] = strtok(NULL, " ");
-		printf("token : %s\n",token[i]);
 	}
 	return (token);
 }
@@ -30,10 +34,9 @@ char **token(char *str)
 int main()
 {
 	char *str;
-	size_t size = 100;
+	size_t size = ARGS * ARGS_SIZE;
 	char **command;
 	int j;
-	const int arg_size = 10;
 
 	printf("#shell_alx$ ");
 	/*We print this at every command line*/
@@ -42,14 +45,15 @@ int main()
 	/*We get a string containing the command*/
 
 	command = token(str);
-	if (strcmp(command[0], "pid\0") == 0)
+	/*We divide the string into separate arguments/tokens */
+	if (strcmp(command[0], "pid") == 0)
 			execve("./pid", command, NULL);
 	else
 			printf("Command does not exist\n");
 
 	free(str);
 	/*insert code to free command[i] for all i*/
-	for (j = 0; j < arg_size; j++)
+	for (j = 0; j < ARGS_SIZE; j++)
 		free(command[j]);
 	free(command);
 	return (0);
